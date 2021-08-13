e9abf92f7bfd20b68b4a19c9ef96ef04123c0cea
Alla Kobyzeva <apkobyzeva@yandex.ru>
2021-08-13 22:55:58 +0300

    Доработка

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -1,34 +1,34 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderCardTest {
 
     public String deliveryDate(int days) {
-        LocalDate date = LocalDate.now();
-        LocalDate newDate = date.plusDays(days);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dateNewFormat = dateFormat.format(newDate);
-        return dateNewFormat;
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void shouldOrderCardWithFullData() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Москва");;
-        $("[data-test-id=date] input").clear();
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(deliveryDate(5));
         $("[name='name']").setValue("Василий Иванов");
         $("[name='phone']").setValue("+79200000000");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText(deliveryDate(5))).shouldBe(visible, Duration.ofSeconds(15));
     }
 }
